72154b4fd6201fc8dedab3759041a7c141549cd7
简律纯 <Hsiangnianian@outlook.com>
2023-01-25 12:09:59 +0800

    Update and rename setup.cfg to setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "rb") as fh:
+    long_description = fh.read().decode("utf-8")
+
+setuptools.setup(
+    name='retrofor_wut',
+    version='0.2.3',
+    url='https://github.com/retrofor/retrofor_wut',
+    license='GPL 3.0',
+    author='简律纯',
+    author_email='HsiangNianian@outlook.com',
+    description='Python codes to Flowcharts.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
+    ],
+    python_requires='>=3.8',
+    install_requires=['astunparse', 'chardet'],
+)
