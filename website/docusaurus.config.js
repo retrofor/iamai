743e418910e6a66aeac476a08b5d0451ea4b6c76
简律纯 <admin@jyunko.cn>
2023-02-05 01:16:26 +0800

    🐱‍🏍fix bug about config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,20 +6,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'retrofor',
+  tagline: 'Cross-platform robot framework, mainly used for ML',
+  favicon: 'img/retro.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://iamai.retrofor.space',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'retrofor', // Usually your GitHub org/user name.
+  projectName: 'retro', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
