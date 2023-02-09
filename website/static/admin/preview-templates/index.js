6cab21b1a8cb3029cb3cbf3c37bd8652a82ce266
简律纯 <admin@jyunko.cn>
2023-02-09 23:08:17 +0800

    🐱‍🏍HsiangNianian Update blog “test-2” (#85)
    Automatically generated. Merged on Netlify CMS.

--- website/static/admin/preview-templates/index.js
+++ website/static/admin/preview-templates/index.js
@@ -8,11 +8,11 @@ CMS.registerPreviewTemplate("pages", Page);
 CMS.registerPreviewStyle("/_includes/assets/css/inline.css");
 // Register any CSS file on the home page as a preview style
 fetch("/")
-  .then(response => response.text())
-  .then(html => {
+  .then((response) => response.text())
+  .then((html) => {
     const f = document.createElement("html");
     f.innerHTML = html;
-    Array.from(f.getElementsByTagName("link")).forEach(tag => {
+    Array.from(f.getElementsByTagName("link")).forEach((tag) => {
       if (tag.rel == "stylesheet" && !tag.media) {
         CMS.registerPreviewStyle(tag.href);
       }
