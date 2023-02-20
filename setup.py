af3806fa4937da3f6f753923e392b7dbb33ae6c9
简律纯 <admin@jyunko.cn>
2023-02-21 01:11:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "rb") as fh:
 
 setuptools.setup(
     name="iamai",
-    version="5.0.5",
+    version="3.1",
     url="https://github.com/retrofor/iamai",
     license="MIT",
     author="简律纯",
