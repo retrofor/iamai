6c8f7b7cf0f5f2db832cac4fd1c02dc7e9507921
简律纯 <admin@jyunko.cn>
2023-04-30 04:15:47 +0800

    保留日志文件 (#155)
    * 保留日志文件
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update log.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- iamai/log.py
+++ iamai/log.py
@@ -3,23 +3,27 @@
 iamai 使用 [loguru](https://github.com/Delgan/loguru) 来记录日志信息。
 自定义 logger 请参考 [loguru](https://github.com/Delgan/loguru) 文档。
 """
+import os
 import sys
+from datetime import datetime
 
 from loguru import logger as _logger
 
 logger = _logger
+
+current_path = os.path.dirname(os.path.abspath("__file__"))
+log_path = os.path.join(
+    current_path, "logs", datetime.now().strftime("%Y-%m-%d") + ".log"
+)
 
 
 def error_or_exception(message: str, exception: Exception, verbose: bool):
     logger.remove()
     logger.add(
         sys.stderr,
-<<<<<<< HEAD
-        format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> [<level>{level}</level>] > <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
-=======
         format="<magenta>{time:YYYY-MM-DD HH:mm:ss.SSS}</magenta> <level>[{level}]</level> > <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
->>>>>>> 0ee2714126501b7068d8b0e65b8d3d4fb815cf16
     )
+    logger.add(sink=log_path, level="INFO", rotation="10 MB")  # 每个日志文件最大为 10MB
     if verbose:
         logger.exception(message)
     else:
