e2bbdfd3bb8aa495e88a6373f75012eeb3a59042
简律纯 <i@jyunko.cn>
2023-07-29 10:02:16 +0800

    feat: 添加消息事件基类的几个快捷方法 get() reply() ask()

--- iamai/event.py
+++ iamai/event.py
@@ -2,16 +2,15 @@
 
 事件类的基类。适配器开发者应实现此事件类基类的子类。
 """
-from abc import ABC
-from typing import Any, Generic, Optional
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any, Generic, Optional, Union
+from typing_extensions import Self
 
-from pydantic import BaseModel, PrivateAttr
+from pydantic import BaseModel, ConfigDict
 
-from iamai.message import Message
 from iamai.typing import T_Adapter
-from iamai.utils import DataclassEncoder
 
-__all__ = ["Event"]
+__all__ = ["Event", "MessageEvent"]
 
 
 class Event(ABC, BaseModel, Generic[T_Adapter]):
@@ -20,46 +19,96 @@ class Event(ABC, BaseModel, Generic[T_Adapter]):
     Attributes:
         adapter: 产生当前事件的适配器对象。
         type: 事件类型。
-        __handled__: 表示事件是否被处理过了，用于适配器处理。
-            警告：请勿手动更改此属性的值。
+        __handled__: 表示事件是否被处理过了，用于适配器处理。警告：请勿手动更改此属性的值。
     """
 
-    adapter: Any
-    # 这里的 adapter 类型定义只是为了 IDE 的类型检查工具能够正常工作，这个字段将永远不会被实际使用
-    # IDE 对 BaseModel 实例化时的提示会将 BaseModel 视为 dataclasses，而忽略 __init__ 定义
-    # 因此需要此定义防止类型提示出错
-    # 参考：
-    # https://pydantic-docs.helpmanual.io/visual_studio_code/#technical-details
-    # https://koxudaxi.github.io/pydantic-pycharm-plugin/ignore-init-arguments/
+    model_config = ConfigDict(extra="allow")
 
+    if TYPE_CHECKING:
+        adapter: T_Adapter
+    else:
+        adapter: Any
     type: Optional[str]
-
-    _adapter: Optional[T_Adapter] = PrivateAttr()  # adapter 实际上放在这里
-    __handled__: bool = PrivateAttr(default=False)
-
-    def __init__(self, **data):
-        if "adapter" in data:
-            self._adapter = data.pop("adapter")
-        else:
-            raise ValueError('"adapter" argument must be set')
-        super().__init__(**data)
-
-    @property
-    def adapter(self) -> T_Adapter:
-        """产生当前事件的适配器对象。"""
-        return self._adapter
+    __handled__: bool = False
 
     def __str__(self) -> str:
+        """返回事件的文本表示。
+
+        Returns:
+            事件的文本表示。
+        """
         return f"Event<{self.type}>"
 
     def __repr__(self) -> str:
+        """返回事件的描述。
+
+        Returns:
+            事件的描述。
+        """
         return self.__str__()
 
-    class Config:
-        extra = "allow"
-        json_encoders = {Message: DataclassEncoder}
 
-    async def query(
+class MessageEvent(Event[T_Adapter], Generic[T_Adapter]):
+    """通用的消息事件类的基类。"""
+
+    @abstractmethod
+    def get_plain_text(self) -> str:
+        """获取消息的纯文本内容。
+
+        Returns:
+            消息的纯文本内容。
+        """
+
+    @abstractmethod
+    async def reply(self, message: str, *args: Any, **kwargs: Any) -> Any:  # noqa: D417
+        """回复消息。
+
+        Args:
+            message: 回复消息的内容。
+
+        Returns:
+            回复消息动作的响应。
+        """
+
+    @abstractmethod
+    async def is_same_sender(self, other: Self) -> bool:
+        """判断自身和另一个事件是否是同一个发送者。
+
+        Args:
+            other: 另一个事件。
+
+        Returns:
+            是否是同一个发送者。
+        """
+
+    async def get(
+        self,
+        *,
+        max_try_times: Optional[int] = None,
+        timeout: Optional[Union[int, float]] = None,
+    ) -> Self:
+        """获取用户回复消息。
+
+        相当于 `Bot` 的 `get()`，条件为适配器、事件类型、发送人相同。
+
+        Args:
+            max_try_times: 最大事件数。
+            timeout: 超时时间。
+
+        Returns:
+            用户回复的消息事件。
+
+        Raises:
+            GetEventTimeout: 超过最大事件数或超时。
+        """
+        return await self.adapter.get(
+            self.is_same_sender,
+            event_type=type(self),
+            max_try_times=max_try_times,
+            timeout=timeout,
+        )
+
+    async def ask(
         self,
         message: str,
         max_try_times: Optional[int] = None,
