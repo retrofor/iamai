c313c0bbac965fb28e84bfe1701176ce2baef50b
简律纯 <i@jyunko.cn>
2023-08-09 14:28:56 +0800

    refactor: sh

--- docs/scripts/build-api.sh
+++ docs/scripts/build-api.sh
@@ -2,13 +2,21 @@
 
 # Checkout the repository
 git clone https://github.com/$VERCEL_GIT_REPO_SLUG.git
-cd iamai
+cd your-repository-name
 
 # Install dependencies
+pip install --upgrade pdm
 pdm install -G dev -G docs.old
 
 # Build API Doc
-pdm run sophia-doc iamai -o docs/pages/dev-api --anchor-extend --ignore-data --overwrite --exclude-module-name --init-file-name index.md
+pdm run sophia-doc iamai -o docs.old/docs/dev-api --anchor-extend --ignore-data --overwrite --exclude-module-name --init-file-name index.md
 export IAMAI_DEV=1
 
-npm build
+# Install dependencies for VuePress site
+cd docs.old
+pnpm install
+
+# Build VuePress site
+pnpm docs:build
+
+# Deploy to Vercel (automatically handled by Vercel)
