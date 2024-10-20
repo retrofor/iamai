cc4afd61140a29dd3c55caf2ae7c35a4678622dd
HsiangNianian <admin@asynth.in>
2024-10-20 10:40:49 +0800

    feat: remove git-webhook code and related config in examples

--- examples/plugins/_git-webhook/__init__.py
+++ examples/plugins/_git-webhook/__init__.py
@@ -76,8 +76,3 @@ class F(Plugin):
                 return EVENT_DESCRIPTIONS[event_type].format(**data)
         except KeyError:
             return None
-
-
-"""
->smee -u https://smee.io/MHldOrjI8msgNUa5 -p 8888
-"""
