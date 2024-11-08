a8957cd408352437de774b115880c178ecb674c2
HsiangNianian <admin@asynth.in>
2024-11-09 07:51:18 +0800

    feat(iamai/cli): add internationalization support and optimize output messages

--- iamai/cli.py
+++ iamai/cli.py
@@ -1,24 +1,28 @@
 import click
 import asyncio
+import gettext
+import os
 
 from .const import __version__
 
+# Set up message catalog access
+localedir = os.path.join(os.path.dirname(__file__), 'locale')
+gettext.bindtextdomain('messages', localedir)
+gettext.textdomain('messages')
+_ = gettext.gettext
 
 @click.group()
 def cli():
     pass
 
-
 @cli.command()
 @click.argument("plugin_name", required=True)
 def install(plugin_name):
-    click.echo(f"正在安装 {plugin_name} 插件...")
-
+    click.echo(_("Installing plugin: {plugin_name}").format(plugin_name=plugin_name))
 
 @cli.command()
 def version():
-    click.echo(f"当前版本：{__version__}")
-
+    click.echo(_("Current version: {version}").format(version=__version__))
 
 def cli_func(*args):
     cli(*args)
