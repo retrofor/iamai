2d099f9e0b0f2adc8d667f1b8690b57e43117f6d
HsiangNianian <i@jyunko.cn>
2024-12-19 21:52:30 +0800

    feat(engine, plugin): add engine and plugin support in Python module

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,34 @@
 use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use pyo3::wrap_pymodule;
+
+pub mod engine;
+pub mod plugin;
+pub mod mapper;
+pub mod lang;
+
+
+use crate::engine::Engine;
+use crate::plugin::WebSocketPlugin;
+
+#[pymodule]
+fn my_rust_module(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Engine>()?;
+    m.add_function(wrap_pyfunction!(run_engine, m)?)?;
+    Ok(())
+}
 
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn run_engine(py: Python, host: String, port: u16) -> PyResult<()> {
+    let engine = Engine::new(vec![WebSocketPlugin::new()]);
+    engine.run(host, port);
+    Ok(())
 }
+
 
 #[pymodule]
 fn _core(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_submodule(wrap_pymodule!(my_rust_module, m)?)?;
+    m.add_function(wrap_pyfunction!(run_engine, m)?)?;
     Ok(())
 }
